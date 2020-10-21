df41d922ab826a9efce203881ff7fabe539381e6
ffd-sys-team <papeli.krishna.osv@fedex.com>
2020-10-21 07:57:21 +0000

    updated runner file

--- src/test/java/TestRunner/Runner.java
+++ src/test/java/TestRunner/Runner.java
@@ -21,9 +21,9 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 @CucumberOptions(
 		strict = true,
 		dryRun = false,
-		tags={"@tag1011,@tag1020"},
+		//tags={"@tag1011,@tag1020"},
 		//tags={"@tag1013,@tag1014,@tag1015,@tag1016,@tag1017,@tag1018,@tag1019,@tag1020,@tag1021,@tag1022,@tag1023"},
-				//tags={"@tag5"},
+				tags={"@tag5"},
 		features = {"src\\test\\java\\featureFile"},
 		//features = {"src\\test\\java\\featureFile"},
 		glue = {"StepDefinition"},
