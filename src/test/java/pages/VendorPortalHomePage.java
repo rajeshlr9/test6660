d73e5b06349cd5246f41f480db3dec882c7bf82c
5896588 <rakesh.nayak.osv@fedex.com>
2024-07-02 13:09:03 +0000

    updated EDi files

--- src/test/java/pages/VendorPortalHomePage.java
+++ src/test/java/pages/VendorPortalHomePage.java
@@ -19,7 +19,7 @@ import utils.SeleniumTestHelper;
 import utils.SeleniumTestHelper.DropDownMode;
 
 public class VendorPortalHomePage {
-	
+
 	WebDriver driver;
 
 	public static String TestedBy = null;
@@ -31,86 +31,86 @@ public class VendorPortalHomePage {
 		this.driver = Steps.seleniumDriver;
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	@FindBy(xpath = "//div[@id='applications']//ul//li/a[contains(text(),'Vendor')]")
 	public WebElement VendorTab;
 
 	@FindBy(xpath = "//li[@id='receiveListItem']")
 	public WebElement receiveTab;
-	
+
 	@FindBy(xpath = "//div[@id='warehouseDetailTable_filter']//input[@type='search']")
 	public WebElement nodeFilterField;
-	
+
 	@FindBy(xpath = "//select[@id='reasonCodeSelect']")
 	public WebElement reasonCodeSelector;
-	
+
 	@FindBy(xpath = "//div[@id='Name']//input")
 	public WebElement custName;
-	
+
 	@FindBy(xpath = "//select[@id='enterprise']")
 	public WebElement selectEnterPrise;
-	
+
 	@FindBy(xpath = "//input[@name='receiptNbr']")
 	public WebElement receiptNumber;
-	
+
 	@FindBy(xpath = "//table[@id='warehouseDetailTable']//tr[1]/td//a[contains(text(),'AMIAM')]")
 	public WebElement tableDataWithNodeAMIAM;
-	
+
 	@FindBy(xpath = "//button[@id='searchButton']")
 	public WebElement loadShipmentButton;
-	
+
 	@FindBy(xpath = "//table[@id='receiveTable']/tbody/tr[contains(@class,'fsc-rcv-tbl-grp-header')]")
 	public WebElement expASNNumInSearchResult;
-	
+
 	@FindBy(xpath = "//table[@id='receiveTable']/tbody/tr[contains(@class,'fx-odd')]/td[2]")
 	public WebElement statusInSearchResult;
-	
+
 	@FindBy(xpath = "//table[@id='receiveTable']/tbody/tr[contains(@class,'fx-odd')]/td[3]")
 	public WebElement itemIdInSearchResult;
-	
+
 	@FindBy(xpath = "//table[@id='receiveTable']/tbody/tr[contains(@class,'fx-odd')]/td[7]")
 	public WebElement uOMInSearchResult;
-	
+
 	@FindBy(xpath = "//table[@id='receiveTable']/tbody/tr[contains(@class,'fx-odd')]/td[9]//input")
 	public WebElement itemQtyInSearchResult;
-	
+
 	@FindBy(xpath = "//table[@id='receiveTable']/tbody/tr[contains(@class,'fx-odd')]/td[4]")
 	public WebElement orderNumInSearchResult;
-	
+
 	@FindBy(xpath = "//input[@name='lotNbr']")
 	public WebElement lotNumber;
-	
+
 	@FindBy(xpath = "//input[@id='LOTNO_0_0']")
 	public WebElement lotNumInSearchResult;
-	
+
 	@FindBy(xpath = "//input[@name='serialNbr']")
 	public WebElement serialNumber;
-	
+
 	@FindBy(xpath = "//input[@id='SERIALNO_0_0']")
 	public WebElement serialNumInSearchResult;
-	
+
 	@FindBy(xpath = "//div[contains(@class,'fx-error-banner')]/p")
 	public WebElement errorBanner;
-	
+
 	@FindBy(xpath = "//*[@id='receiveTable']/tbody/tr[2]/td[10]/input")
 	public WebElement putawayLocation;
-	
+
 	@FindBy(xpath = "//*[@id='receiveTable']")
 	public WebElement receiveTable;
-	
+
 	@FindBy(xpath = "//button[@id='receiveButton']")
 	public WebElement receiveButton;
-	
+
 	@FindBy(xpath = "//table[@id='receiveTable']//following::td[12]")
 	public WebElement receiveStatus;
-	
+
 	@FindBy(xpath = "//input[@name='shipmentNbr']")
 	public WebElement shipmentNbr;
-	
+
 	@FindBy(xpath = "//input[@id='SHIPBYDATE_0_1']")
 	public WebElement shipmentByDt;
-	
-		
+
+
 	public void naviagateAndClickReceiveTab() throws InterruptedException {
 		Thread.sleep(5000);
 		SeleniumTestHelper.waitForElementToBeDisplayed(driver, VendorTab, 180);
@@ -122,7 +122,7 @@ public class VendorPortalHomePage {
 		SeleniumTestHelper.clickOnButton(receiveTab);
 		Thread.sleep(10000);
 	}
-	
+
 	public void enterMandatoryDetails() throws InterruptedException {
 		Thread.sleep(10000);
 		System.out.println("Enter Mandatory Details");
@@ -154,13 +154,13 @@ public class VendorPortalHomePage {
 		SeleniumTestHelper.waitForElementToBeDisplayed(driver, selectEnterPrise, 180);
 		SeleniumTestHelper.scrollToElement(driver, selectEnterPrise);
 		if (account.equalsIgnoreCase("ATM")) {
-		SeleniumTestHelper.selectFromDropDown(selectEnterPrise, "ATM", DropDownMode.VALUE);
+			SeleniumTestHelper.selectFromDropDown(selectEnterPrise, "ATM", DropDownMode.VALUE);
 		}else if (account.equalsIgnoreCase("COM")) {
 			SeleniumTestHelper.selectFromDropDown(selectEnterPrise, "COM", DropDownMode.VALUE);
 		}
 		Thread.sleep(2000);
 	}
-	
+
 	public void searchUsingReceiptNumber() throws InterruptedException {
 		Thread.sleep(2000);
 		SeleniumTestHelper.waitForElementToBeDisplayed(driver, receiptNumber, 180);
@@ -168,7 +168,7 @@ public class VendorPortalHomePage {
 		SeleniumTestHelper.enterText(receiptNumber, Items.getAsnNumber());
 		clickOnLoadShipment();
 	}
-	
+
 	public void searchUsingShipmentNumber() throws InterruptedException {
 		Thread.sleep(2000);
 		SeleniumTestHelper.waitForElementToBeDisplayed(driver, shipmentNbr, 180);
@@ -176,16 +176,16 @@ public class VendorPortalHomePage {
 		SeleniumTestHelper.enterText(shipmentNbr, Items.getPONumber());
 		clickOnLoadShipment();
 	}
-	
+
 	public void searchUsingLotNumber() throws InterruptedException {
 		Thread.sleep(2000);
 		System.out.println("Enter Lot Number");
 		SeleniumTestHelper.waitForElementToBeDisplayed(driver, lotNumber, 180);
 		SeleniumTestHelper.scrollToElement(driver, lotNumber);
 		SeleniumTestHelper.enterText(lotNumber, Items.getLotNumber());
-//		SeleniumTestHelper.waitForElementToBeDisplayed(driver, receiptNumber, 180);
-//		SeleniumTestHelper.scrollToElement(driver, receiptNumber);
-//		SeleniumTestHelper.enterText(receiptNumber, Items.getAsnNumber());
+		//		SeleniumTestHelper.waitForElementToBeDisplayed(driver, receiptNumber, 180);
+		//		SeleniumTestHelper.scrollToElement(driver, receiptNumber);
+		//		SeleniumTestHelper.enterText(receiptNumber, Items.getAsnNumber());
 		SeleniumTestHelper.waitForElementToBeDisplayed(driver, shipmentNbr, 180);
 		SeleniumTestHelper.scrollToElement(driver, shipmentNbr);
 		SeleniumTestHelper.enterText(shipmentNbr, Items.getPONumber());
@@ -197,17 +197,17 @@ public class VendorPortalHomePage {
 		SeleniumTestHelper.waitForElementToBeDisplayed(driver, serialNumber, 180);
 		SeleniumTestHelper.scrollToElement(driver, serialNumber);
 		SeleniumTestHelper.enterText(serialNumber, Items.getEdiSerialNumber());
-//		SeleniumTestHelper.waitForElementToBeDisplayed(driver, receiptNumber, 180);
-//		SeleniumTestHelper.scrollToElement(driver, receiptNumber);
-//		SeleniumTestHelper.enterText(receiptNumber, Items.getAsnNumber());
+		//		SeleniumTestHelper.waitForElementToBeDisplayed(driver, receiptNumber, 180);
+		//		SeleniumTestHelper.scrollToElement(driver, receiptNumber);
+		//		SeleniumTestHelper.enterText(receiptNumber, Items.getAsnNumber());
 		clickOnLoadShipment();
 	}
-	
+
 	public void validateDataInReceiveTable() throws Exception {
 		System.out.println("Validate the Expected data in Receive Table");
 		SeleniumTestHelper.waitForElementToBeDisplayed(driver, expASNNumInSearchResult, 300);
 		SeleniumTestHelper.scrollToElement(driver, expASNNumInSearchResult);
-		
+
 		System.out.println("Expected shipmentVal: "+Items.getPONumber());
 		System.out.println("Actual shipmentVal: "+expASNNumInSearchResult.getText());
 		System.out.println("Item: "+Steps.ItemDataMap.get(0).get("Item"));
@@ -221,7 +221,7 @@ public class VendorPortalHomePage {
 		}else if(Steps.scenarioData.get("Order Source").equals("FedexNet")) {
 			Assert.assertEquals(Items.getPONumber().contains(expASNNumInSearchResult.getText()), true, "Actual and Expected ASN Number didn't matched");
 		}
-		
+
 		Assert.assertEquals(statusInSearchResult.getText(), "Shipped", "Actual and Expected Status didn't matched");
 		Assert.assertEquals(itemIdInSearchResult.getText(), Steps.ItemDataMap.get(0).get("Item"), "Actual and Expected item didn't matched");
 		Assert.assertEquals(uOMInSearchResult.getText(), Steps.ItemDataMap.get(0).get("UOM"), "Actual and Expected UOM didn't matched");
@@ -239,7 +239,7 @@ public class VendorPortalHomePage {
 		Reporter.addStepLog("Lot Number: " + Items.getLotNumber());
 		Thread.sleep(5000);
 	}
-	
+
 	public void validateSerialNumberInReceiveTable() throws InterruptedException {
 		System.out.println("Validate Serial number in receive table");
 		SeleniumTestHelper.waitForElementToBeDisplayed(driver, serialNumInSearchResult, 180);
@@ -251,9 +251,9 @@ public class VendorPortalHomePage {
 		Reporter.addStepLog("Serial Number: " + Items.getEdiSerialNumber());
 		Thread.sleep(5000);
 	}
-	
+
 	public void clickOnLoadShipment() throws InterruptedException {
-		
+
 		Thread.sleep(2000);
 		System.out.println("Click on Load Shipment");
 		SeleniumTestHelper.waitForElementToBeDisplayed(driver, loadShipmentButton, 180);
@@ -265,36 +265,59 @@ public class VendorPortalHomePage {
 			Assert.assertTrue(false, errorBanner.getText());
 		}
 	}
-	
+
 	public void vpReceive() throws InterruptedException {
 		int rows =  driver.findElements(By.xpath("//table[@id='receiveTable']/tbody//following::tr")).size();
 		for(int i=2;i<=rows;i++) {
 			driver.findElement(By.xpath("//*[@id='receiveTable']/tbody/tr["+i+"]/td[10]/input")).click();
-			 Thread.sleep(2000);
+			Thread.sleep(2000);
 			driver.findElement(By.xpath("//*[@id='receiveTable']/tbody/tr["+i+"]/td[10]/input")).sendKeys(Steps.ItemDataMap.get(i-2).get("RecLocation"));
-			 Thread.sleep(2000);
-			 if(Steps.ItemDataMap.get(i-2).get("ShipByDate")!="") {
-				 shipmentByDt.sendKeys("11112035");
-			 }
+			Thread.sleep(2000);
+			if(Steps.ItemDataMap.get(i-2).get("ShipByDate")!="") {
+				shipmentByDt.sendKeys("11112035");
+			}
 			//putawayLocation.click();
 			//putawayLocation.sendKeys(Steps.ItemDataMap.get(i-2).get("RecLocation"));
 		}
-		
+
 		receiveButton.click();
-		 Thread.sleep(2000);
+		Thread.sleep(2000);
 		String status = "Complete";
 		
-		 String AsnreceiveStatus = receiveStatus.getText();
-			int count = 0;
-			  while (!AsnreceiveStatus.equals(status) && (count != 20)) { 
+		//String AsnreceiveStatus = receiveStatus.getText();
+		
+		int rowCount = 0;
+		// System.out.println("AsnreceiveStatus:"+AsnreceiveStatus);
+		//Assert.assertEquals( AsnreceiveStatus,status);
 
-			  AsnreceiveStatus = receiveStatus.getText();
-			  Thread.sleep(6000);			 
-			  count++; 
-			  }
-			  System.out.println("AsnreceiveStatus:"+AsnreceiveStatus);
-			  Assert.assertEquals( AsnreceiveStatus,status);
-		
+		System.out.println("rows: "+rows);
+		for(int i=2;i<=rows;i++) {
+			int count = 0;
+			String rowStatus = "";
+			System.out.println("rowVal: "+i);
+			while (!rowStatus.equals(status) && (count != 20)) { 
+				rowStatus = driver.findElement(By.xpath("//*[@id='receiveTable']/tbody/tr["+i+"]/td[11]")).getText();
+				System.out.println("count: "+count+" rowStatus: "+rowStatus);
+				Thread.sleep(6000);			 
+				count++; 
+				
+				if(rowStatus.equals(status)) {
+					rowCount++; 
+					System.out.println("rowCount: "+rowCount);
+				}
+			}
+
+
+		}
+		if(rowCount==rows-1) {
+			System.out.println("here1");
+			Assert.assertTrue(true);
+			
+
+		}else {
+			System.out.println("here2");
+			Assert.assertTrue(false);
+		}
 	}
-		
+
 }
