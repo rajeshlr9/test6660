d57b4483143615cedaa5dacca46ef31aae66e101
pam_npcilmanwmsadm10 <pam_npcilmanwmsadm10@corp.ds.fedex.com>
2024-07-24 12:01:09 +0000

    Updated APC Script

--- src/test/java/TestRunner/Runner.java
+++ src/test/java/TestRunner/Runner.java
@@ -22,7 +22,7 @@ import utils.ExtentReportUpdate;
 @CucumberOptions(
 		strict = true,
 		dryRun = false,
-		tags={"@OutBoundScenario_APC_1"}, 
+		tags={"@tag5"}, 
 				
 		features = {"src\\test\\java\\featureFile"},
 		glue = {"StepDefinition"},
